5702f580816d20cda45b42bacbb291b833cd9865
surendra277 <surendrathore277@gmail.com>
2024-06-20 08:30:04 +0530

    Add new code

--- bookstore_client/src/Home/Home.jsx
+++ bookstore_client/src/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Banner from '../component/Banner'
 
 const Home = () => {
   return (
-    <div>
-      <div className='h-screen'>Home</div>
-      <div className="h-screen bg-red-600"></div>
-    </div>
+    <>
+      {/* <div className='h-screen'>Home</div> */}
+      {/* <div className="h-screen bg-red-600"></div> */}
+      <Banner />
+    </>
   )
 }
 
