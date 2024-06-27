6bcdf6f76e9bd5fee233466300fa9fdae74d190b
surendra277 <surendrathore277@gmail.com>
2024-06-27 08:38:04 +0530

    new update

--- bookstore_client/src/component/SignUp.jsx
+++ bookstore_client/src/component/SignUp.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 const SignUp = () => {
+
+  const handleSignUp = (event)=>{
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const username = form.un.value;
+    
+    const password = form.password.value;
+    console.log(username,email,password);
+  }
+
   return (
     <>
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
@@ -16,18 +28,18 @@ const SignUp = () => {
               <h1 className="text-2xl font-semibold">SignUp</h1>
             </div>
             <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+              <form onSubmit={handleSignUp} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <div className="relative">
-                  <input autocomplete="off" name="un" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="User Name" />
-                  <label for="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">User Name</label>
+                  <input  name="un" type="text" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="User Name" />
+                  
                 </div>
                 <div className="relative">
-                  <input autocomplete="off" name="un" type="email" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email" />
-                  <label for="Email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email</label>
+                  <input  name="email" type="email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email" />
+                  
                 </div>
                 <div className="relative">
-                  <input autocomplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-                  <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                  <input   name="password" type="password" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                 
                 </div>
                 <div className="relative">
                   <button className="bg-cyan-500 text-white rounded-md px-2 py-1">Submit</button>
@@ -41,7 +53,7 @@ const SignUp = () => {
 </button>
 </div>
  
-              </div>
+              </form>
 
             </div>
           </div>
