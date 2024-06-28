9d2a55b9a5b4137169a7277213e71720e9598846
dheerajs7 <dheeraj.salvi293@gmail.com>
2024-06-28 10:32:26 +0530

    added book data

--- book-server/controller/book.controller.js
+++ book-server/controller/book.controller.js
@@ -3,24 +3,57 @@ import Book from "../model/book.model.js";
 const bookController={
   
       async storeBook(req,res){
+        const { title, author, description, price, imageURL, categories, stock } = req.body;
+
+        const newBook = new Book({
+            title,
+            author,
+            description,
+            price,
+            imageURL,
+            categories,
+            stock
+        });
+      
         try {
-            const{name,price,category,image,title}=req.body;
-            const book = await Book.create({name,price,category,image,title}); 
-            return res.status(201).json(book);
-            } catch (error) {
-                return res.status(400).json({error: error.message});
-                }
+            const savedBook = await newBook.save();
+            res.status(201).json(savedBook);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
       },
 
     async getBook(req,res){
-    try {
-        const book = await Book.find();
-        res.status(200).json(book);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+      try {
+        const books = await Book.find();
+        res.json(books);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-}
+},
+async updateBook(req,res,next){
+    let book;
+    try{
+      const{id}=req.params;
+      const updateBookData= req.body;
+      book= await Book.findByIdAndUpdate({_id:id},updateBookData);
+     
+    }
+    catch(err){
+       res.status(404).json({err:"Server Error",serverError:err})
+    }
+    res.status(201).json(book);  
+  },
+  
+  async deleteBook(req,res,next){
+    try {
+      await Book.findByIdAndDelete(req.params.id);
+      res.json({ message: 'Book deleted' });
+  } catch (err) {
+      res.status(500).json({ message: err.message });
+  }
+  },
+ 
 }
 
 export default bookController;
