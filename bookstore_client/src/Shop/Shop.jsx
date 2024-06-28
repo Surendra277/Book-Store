158c397ff5a4a63d778edb8d26c816f1e518f42d
ajayy4u <rathoreajay326@gmail.com>
2024-06-28 14:06:22 +0530

    about page completed

--- bookstore_client/src/Shop/Shop.jsx
+++ bookstore_client/src/Shop/Shop.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import { Card } from "flowbite-react";
 
 const Shop = () => {
-  return ( 
-    <div>Shop</div>
+  const[books, setBooks] = useState([]);
+
+  useEffect( () => {
+    fetch("").then(res => res.json()).then(data => setBooks(data));
+  }, [])
+  return (
+    <div className='mt-28 px-4 lg:px-24'>
+      <h2 className='text-5xl font-bold text-center'>All Books are here</h2>
+
+      <div className='grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1'>
+        {books.map((book) => (
+          <Card key={book.id} className="flex flex-col items-center">
+            <img src={book.imageURL} alt={book.bookTitle} className='h-96 w-full object-cover' />
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-4">
+              {book.bookTitle}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400 mt-2">
+              {book.description}
+            </p>
+            <button className='bg-blue-700 font-semibold text-white py-2 px-4 rounded mt-4'>Shop Now</button>
+          </Card>
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default Shop
+export default Shop;
--- bookstore_client/src/component/About.jsx
+++ bookstore_client/src/component/About.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import '../component/About.css';
 import bookstoreImage from '../assets/bookstore.jpg';
-import ownerImage from '../assets/owner.jpg';
-import teamImage from '../assets/team.jpg';
 
 const About = () => {
   return (
@@ -15,7 +13,7 @@ const About = () => {
 
 const HeroSection = () => (
   <div className="hero-section" style={{ backgroundImage: `url(${bookstoreImage})` }}>
-    <h1>Welcome to Our Book-Store</h1>
+    <h1>Welcome to Our BookStore</h1>
   </div>
 );
 
@@ -28,22 +26,18 @@ const ContentSection = () => (
 
 const OwnerSection = () => (
   <div className="owner-section">
-    <img src={ownerImage} alt="Owner" />
-    <div className="text">
+    <div className="text items-center">
       <h2>About the Store</h2>
       <p>
-        Welcome to Bibliomart, your premier destination for all things literary! Founded by book enthusiasts for book enthusiasts, our online bookstore is a haven for readers of all ages and interests. Whether you're a lover of classic literature, a fan of contemporary fiction, or searching for the latest bestsellers, we have something special for everyone.
+        Welcome to Bibliomart, your premier destination for all things literary! Founded by book enthusiasts for book enthusiasts, our online bookstore is a haven for readers of all ages and interests. Whether you're a lover of classic literature, a fan of contemporary fiction, or searching for the latest bestsellers, we have something special for everyone. From classics to contemporary bestsellers, our online bookstore is your gateway to a universe of literature, tailored to every reader's taste.
       </p>
       <h3>Our Mission</h3>
       <p>
         At Bibliomart, our mission is simple: to connect people with the books they love. We believe that reading enriches lives, sparks imagination, and fosters a deeper understanding of the world. Our goal is to provide an extensive, carefully curated selection of books that cater to diverse tastes and preferences.
       </p>
-      <h1>What Sets Us Apart</h1>
+      <h3>What Sets Us Apart</h3>
       <p>
         <strong>Vast Selection:</strong> From timeless classics to modern masterpieces, our collection spans a wide range of genres, including fiction, non-fiction, mystery, romance, science fiction, fantasy, and more. We also offer a selection of rare and hard-to-find titles for the discerning reader.
-      </p>
-      <p>
-        <strong>Expert Recommendations:</strong> Not sure what to read next? Our team of literary experts is here to help! We offer personalized recommendations based on your interests, ensuring you find the perfect book for your next reading adventure.
       </p>
       <p>
         <strong>Exceptional Service:</strong> We pride ourselves on our commitment to customer satisfaction. Our friendly and knowledgeable customer service team is always ready to assist with any questions or concerns, making your shopping experience as smooth and enjoyable as possible.
@@ -52,10 +46,7 @@ const OwnerSection = () => (
         <strong>Fast and Reliable Shipping:</strong> We understand the excitement of receiving a new book, which is why we offer fast and reliable shipping options. Our carefully packaged orders ensure your books arrive in pristine condition, ready to be enjoyed.
       </p>
       <p>
-        <strong>Community Focus:</strong> We believe in the power of books to bring people together. That's why we regularly host virtual book clubs, author Q&A sessions, and other community events that celebrate the joy of reading.
-      </p>
-      <p>
-        Join Our Community
+        <strong>Join Our Community</strong>
         At Bibliomart, we're more than just a bookstore; we're a community of readers, writers, and book lovers. We invite you to explore our collection, join our events, and share your love of books with us. Thank you for choosing Bibliomart as your trusted source for all your reading needs.
       </p>
     </div>
@@ -64,12 +55,22 @@ const OwnerSection = () => (
 
 const TeamSection = () => (
   <div className="team-section">
-    <img src={teamImage} alt="Team" />
     <div className="text">
       <h2>Meet Our Team</h2>
       <p>
-        Our dedicated team of book experts is here to help you find your next great read. Whether you're looking for a classic novel, the latest bestseller, or a hidden gem, we're here to assist you.
+        At Bibliomart, we believe that great things are achieved through the collective effort of dedicated individuals. Our team is the heart of our success, working together to bring you an exceptional online bookstore experience.
+        Whether you're looking for a classic novel, the latest bestseller, or a hidden gem, we're here to assist you.
       </p>
+      <p>
+        By fostering a culture of collaboration, we encourage innovative ideas that enhance our services and products. Whether it's curating the latest book collections or developing user-friendly website features.
+        Behind every successful online bookstore is a well-coordinated team managing operations smoothly.
+      </p>
+
+      <p>
+        At Bibliomart, we believe in the power of continuous learning. Through regular training sessions and knowledge-sharing meetings, our team members stay updated with industry trends and best practices. 
+        Teamwork extends beyond our bookstore. We actively engage with our community of readers, authors, and partners, building strong relationships that enrich the literary ecosystem. By supporting each other, we create a thriving environment where everyone benefits.
+      </p>
+
     </div>
   </div>
 );
