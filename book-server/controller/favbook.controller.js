8e4bf7129f1368b9bb4beee4f95b71a3c2b245f4
dheerajs7 <dheeraj.salvi293@gmail.com>
2024-07-10 12:42:58 +0530

    favobooks,orders

--- book-server/controller/favbook.controller.js
+++ book-server/controller/favbook.controller.js
@@ -1,25 +1,50 @@
-import BestBook from "../model/favbooks.model.js";
+import User from "../model/user.model.js";
 
-const favbooksController={
-   
-    async storeFavBooks(req, res){
-        try {
-            const{name,price,author,image}=req.body;
-            const favbooks = await BestBook.create({name,price,author,image});
-            res.status(200).json(favbooks);
-            } catch (error) {
-                res.status(404).json({ message: error.message });
-                }
-            },
+const favbooksController = {
+  async storeFavBooks(req, res) {
+    try {
+      const { bookid, id } = req.headers;
+      const userData = await User.findById(id);
+      const isBookFav = userData.favourites.includes(bookid);
+      if (isBookFav) {
+        return res
+          .status(400)
+          .json({ message: "Book is already in your favourites list." });
+      }
+      await User.findByIdAndUpdate(id, { $push: { favourites: bookid } });
+      return res.status(200).json({ message: "Book added to Favorite list" });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
+  async removeFavBooks(req, res) {
+    try {
+      const { bookid, id } = req.headers;
+      const userData = await User.findById(id);
+      const isBookFav = userData.favourites.includes(bookid);
+      if (isBookFav) {
+        await User.findByIdAndUpdate(id, { $pull: { favourites: bookid } });
+      }
+     
+      return res.status(200).json({ message: "Book removed from Favorite list" });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
 
-        async getFavBooks(req,res){
-            try {
-                const favbooks = await BestBook.find();
-                res.status(200).json(favbooks);
-                } catch (error) {
-                    res.status(404).json({ message: error.message });
-                    }
-                    }
-                
-        }    
+  async getFavBooks(req,res){
+    try {
+        const { id } = req.headers;
+        const userData = await User.findById(id).populate("favourites"); 
+        const favoriteBook =userData.favourites;
+        console.log(favoriteBook);
+        return res.status(200).json({ favoriteBook });
+
+  }
+  catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+}
 export default favbooksController;
+     
